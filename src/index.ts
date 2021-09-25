5c185975293e45f6e5eaa29bbe65ff44fa17d478
pinetail-a <90510087+pinetail-a@users.noreply.github.com>
2021-09-25 20:42:27 +0900

    撮った写真に地図を合成する

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,45 @@
+import { SSL_OP_ALL } from 'constants';
 import { MakeImage } from './makeImage/makeimage';
+import { PrismaClient } from '@prisma/client';
 
 const imager = new MakeImage();
-imager.make(34.79436637572929, 135.5416257968678);
+imager.make(35.6595327, 139.7445911);
+
+const sharp = require('sharp');
+
+sharp('page-ss.png').resize(400, 400);
+
+sharp('tower.png')
+  .composite([
+    {
+      input: 'clone.png',
+      gravity: 'southeast',
+    },
+  ])
+  .toFile('output.png', (err: string, info: string) => {
+    if (err) {
+      throw err;
+    }
+  });
+
+const prisma = new PrismaClient();
+
+// A `main` function so that you can use async/await
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  });
+
+  // use `console.dir` to print nested objects
+
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
