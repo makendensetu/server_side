1a382758d7fdcc813b2219b81aeced608b450017
inaridiy <inainainariinainari@gmail.com>
2021-09-25 13:09:45 +0900

    puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-import app from './app';
+import puppeteer from 'puppeteer';
 
-const PORT = process.env.PORT || 3000;
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
 
-app.listen(PORT, () => {
-  console.log(`REST API server ready at: http://localhost:${PORT}`);
-});
+  const targetElementSelector = '#tw-container';
+
+  await page.goto('https://google.co.jp');
+  await page.type('input[name="q"]', 'pupperteer 翻訳');
+  await page.click('button[type="submit"]');
+
+  await page.waitForSelector(targetElementSelector);
+
+  await page.screenshot({ path: 'page-ss.png' });
+
+  const clip = await page.evaluate((s) => {
+    const el = document.querySelector(s);
+
+    // エレメントの高さと位置を取得
+    const { width, height, top: y, left: x } = el.getBoundingClientRect();
+    return { width, height, x, y };
+  }, targetElementSelector);
+
+  // スクリーンショットに位置と大きさを指定してclipする
+  await page.screenshot({ clip, path: 'tw-container-ss.png' });
+
+  browser.close();
+})();
